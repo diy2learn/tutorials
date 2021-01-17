6eb24f84e581dbdbd41f96ed9ca38f896fc568d8
diy2learn <bian.tquang@gmail.com>
2021-01-17 15:00:02 +0100

    Revert "refactored backup_workspace_with_pickle.py"
    This reverts commit d09faf0e

--- backup_workspace/backup_pickle.py
+++ backup_workspace/backup_pickle.py
@@ -1,10 +1,4 @@
 import pickle
-import os
-from datetime import datetime
-import logging
-
-
-logger = logging.getLogger(__name__)
 
 
 def is_picklable(obj):
@@ -15,50 +9,20 @@ def is_picklable(obj):
     return True
 
 
-def get_timestamp():
-    """
-    Examples
-    --------
-    >>> print(get_timestamp())
-    20210117T124929
-    """
-    return datetime.now().strftime("%Y%m%dT%H%M%S")
+bk = {}
+for k in dir():
+    obj = globals()[k]
+    if is_picklable(obj):
+        try:
+            bk.update({k: obj})
+        except TypeError:
+            pass
 
+# to save session
+with open('./your_bk.pkl', 'wb') as f:
+    pickle.dump(bk, f)
 
-def save_session(
-        fpath: str = None):
-    if fpath is None:
-        if not os.path.exists('./backup'):
-            os.makedirs('./backup')
-        fpath = f'./backup/backup_pickle.pkl'
+# to load your session
 
-    __bk = {}
-    black_list = ['__bk']
-    keys = list(set(list(globals().keys())) - set(black_list))
-    for k in keys:
-        print('k: ', k)
-        obj = globals()[k]
-        if is_picklable(obj):
-            try:
-                __bk.update({k: obj})
-            except TypeError:
-                pass
-
-    # to save session
-    with open(fpath, 'wb') as f:
-        pickle.dump(__bk, f)
-    logger.info(f"Pickled to {fpath}")
-
-
-def load_session(
-        fpath: str = None
-):
-    if fpath is None:
-        fpath = f'./backup/backup_pickle.pkl'
-    # to load your session
-    with open(fpath, 'rb') as f:
-        bk_restore = pickle.load(f)
-    return bk_restore
-
-
-
+with open('./your_bk.pkl', 'rb') as f:
+    bk_restore = pickle.load(f)
