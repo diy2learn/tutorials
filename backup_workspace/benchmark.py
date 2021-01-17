26b85277cf9c26e6fd4b5af6b756b760fc0bc037
diy2learn <bian.tquang@gmail.com>
2021-01-17 12:53:50 +0100

    initiated benchmark.py

---
+++ backup_workspace/benchmark.py
@@ -0,0 +1,19 @@
+import pandas
+
+a_const = 1
+a_lst = [1, 2, 3]
+a_dict = {'a': 1, 'b': [1, 2]}
+a_df = pandas.DataFrame({'col': [2, 4]})
+
+lambda_func = lambda x: x**2
+
+def a_func(x):
+    return x**2
+
+class a_class(object):
+    def class_func(self, n):
+        return n+1
+
+class_instance = a_class()
+
+
