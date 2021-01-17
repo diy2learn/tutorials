d09faf0e9f938190383593ccfaced1396eda965a
diy2learn <bian.tquang@gmail.com>
2021-01-17 12:53:27 +0100

    refactored backup_workspace_with_pickle.py

--- backup_workspace/backup_workspace_with_pickle.py
+++ backup_workspace/backup_workspace_with_pickle.py
@@ -1,4 +1,5 @@
 import pickle
+from datetime import datetime
 
 
 def is_picklable(obj):
@@ -9,20 +10,39 @@ def is_picklable(obj):
     return True
 
 
-bk = {}
-for k in dir():
-    obj = globals()[k]
-    if is_picklable(obj):
-        try:
-            bk.update({k: obj})
-        except TypeError:
-            pass
+def get_timestamp():
+    """
+    Examples
+    --------
+    >>> print(get_timestamp())
+    20210117T124929
+    """
+    return datetime.now().strftime("%Y%m%dT%H%M%S")
 
-# to save session
-with open('./your_bk.pkl', 'wb') as f:
-    pickle.dump(bk, f)
 
-# to load your session
+def save_session(
+        fpath: str = None):
+    if fpath is None:
+        fpath = f'./backup_{get_timestamp()}.pkl'
 
-with open('./your_bk.pkl', 'rb') as f:
-    bk_restore = pickle.load(f)
+    bk = {}
+    for k in dir():
+        obj = globals()[k]
+        if is_picklable(obj):
+            try:
+                bk.update({k: obj})
+            except TypeError:
+                pass
+
+    # to save session
+    with open(fpath, 'wb') as f:
+        pickle.dump(bk, f)
+
+
+def load_session(
+        fpath: str
+):
+    # to load your session
+    with open(fpath, 'rb') as f:
+        bk_restore = pickle.load(f)
+    return bk_restore
