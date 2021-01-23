627e472c4ecb60a136d9e7ed28c47c2d6b87b59b
diy2learn <bian.tquang@gmail.com>
2021-01-23 08:27:48 +0100

    no comment

--- understand_jit_compiler/calc.py
+++ understand_jit_compiler/calc.py
@@ -1,18 +1,54 @@
 from time import time
 
 
-def calc(n=1000):
-    res = 0
-    for r in range(n):
-        for c in range(n):
-            for z in range(n):
-                res += r - c + z
-    return res
+import numpy as np
+from numba import jit
+
+
+def proc_numpy(x,y,z):
+
+   x = x*2 - ( y * 55 )
+   y = x + y*2
+   z = x + y + 99
+   z = z * ( z - .88 )
+
+   return z
+
+@jit(nopython=True)
+def proc_numba(xx,yy,zz):
+   for j in range(nobs):
+      x, y = xx[j], yy[j]
+
+      x = x*2 - ( y * 55 )
+      y = x + y*2
+      z = x + y + 99
+      z = z * ( z - .88 )
+
+      zz[j] = z
+   return zz
 
 
 if __name__ == "__main__":
+    nobs = 10000000
+    x = np.random.randn(nobs)
+    y = np.random.randn(nobs)
+    z = np.zeros(nobs)
+
     start_time = time()
-    n = 200
-    print(calc(n))
-    run_time = 1e3*(time() - start_time)
-    print(f"run time: {run_time:.3f} ms")
+    res_numpy = proc_numpy(x, y, z)
+    time_numpy = 1e3 * (time() - start_time)
+
+    start_time = time()
+    z = np.zeros(nobs)
+    res_numba = proc_numba(x, y, z)
+    time_numba = 1e3 * (time() - start_time)
+
+    print('res_numpy == res_numba: ', np.all(res_numpy == res_numba))
+    print(f"run time Numpy/Numba [ms]: {time_numpy: .2f}/{time_numba: .2f}")
+
+    start_time = time()
+    z = np.zeros(nobs)
+    res_numba = proc_numba(x, y, z)
+    time_numba = 1e3 * (time() - start_time)
+    print(f'Numba after compiled [ms]: {time_numba: .2f}')
+
