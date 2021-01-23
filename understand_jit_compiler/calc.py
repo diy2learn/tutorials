54a9139a5246a20b6576b5fa0104009c5238d055
diy2learn <bian.tquang@gmail.com>
2021-01-23 08:49:35 +0100

    updated experiment

--- understand_jit_compiler/calc.py
+++ understand_jit_compiler/calc.py
@@ -4,51 +4,62 @@ from time import time
 import numpy as np
 from numba import jit
 
+n = 1000
 
-def proc_numpy(x,y,z):
 
-   x = x*2 - ( y * 55 )
-   y = x + y*2
-   z = x + y + 99
-   z = z * ( z - .88 )
+def loop_inner(a, b):
+    return a + 2 *b
 
-   return z
+
+def proc_numpy(x, y):
+    res = np.zeros(len(x))
+    for i in range(n):
+        for j in range(n):
+            res += np.add(x, y)
+    return res
+
 
 @jit(nopython=True)
-def proc_numba(xx,yy,zz):
-   for j in range(nobs):
-      x, y = xx[j], yy[j]
-
-      x = x*2 - ( y * 55 )
-      y = x + y*2
-      z = x + y + 99
-      z = z * ( z - .88 )
-
-      zz[j] = z
-   return zz
+def proc_numba(x, y):
+    res = np.zeros(len(x))
+    for i in range(n):
+        for j in range(n):
+            res += np.add(x, y)
+    return res
 
 
 if __name__ == "__main__":
-    nobs = 10000000
+    nobs = 100
     x = np.random.randn(nobs)
     y = np.random.randn(nobs)
-    z = np.zeros(nobs)
 
     start_time = time()
-    res_numpy = proc_numpy(x, y, z)
+    res_numpy = proc_numpy(x, y)
     time_numpy = 1e3 * (time() - start_time)
 
     start_time = time()
-    z = np.zeros(nobs)
-    res_numba = proc_numba(x, y, z)
+    res_numba = proc_numba(x, y)
     time_numba = 1e3 * (time() - start_time)
 
     print('res_numpy == res_numba: ', np.all(res_numpy == res_numba))
     print(f"run time Numpy/Numba [ms]: {time_numpy: .2f}/{time_numba: .2f}")
 
     start_time = time()
-    z = np.zeros(nobs)
-    res_numba = proc_numba(x, y, z)
+    res_numba = proc_numba(x, y)
     time_numba = 1e3 * (time() - start_time)
-    print(f'Numba after compiled [ms]: {time_numba: .2f}')
+    print(f'Numba after compiled(1) [ms]: {time_numba: .2f}')
 
+    start_time = time()
+    res_numba = proc_numba(x, y)
+    time_numba = 1e3 * (time() - start_time)
+    print(f'Numba after compiled(2) [ms]: {time_numba: .2f}')
+
+    start_time = time()
+    res_numba = proc_numba(x, y)
+    time_numba = 1e3 * (time() - start_time)
+    print(f'Numba after compiled(3) [ms]: {time_numba: .2f}')
+
+    start_time = time()
+    res_numpy = proc_numpy(x, y)
+    time_numpy = 1e3 * (time() - start_time)
+    print(f'Numpy after first run(1) [ms]: {time_numpy: .2f}')
